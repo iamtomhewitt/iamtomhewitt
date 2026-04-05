bb1e5bf573c73910b0de61d443cba53ba51abb4b
Tom Hewitt <tommhewitt@gmail.com>
2026-04-05 09:52:13 +0100

    remove scores from readme

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 const { generateReadMe } = require('./services/readme');
-const { getGhostHunterScores, getJetDashVrScores, getMostRecentJetDashVrScores, getMostRecentGhostHunterScores } = require('./services/games');
 const { getLastUpdatedRepos, getRepos, getLatestReleases } = require('./services/github');
 const { toFriendlyDate } = require('./services/lib');
 const { getReputation } = require('./services/stackoverflow');
@@ -11,21 +10,13 @@ const run = async () => {
 
   const { lastUpdatedRepos } = getLastUpdatedRepos();
   const { latestReleases } = await getLatestReleases();
-  const { jetDashVrScores } = await getJetDashVrScores();
-  const { ghostHunterScores } = await getGhostHunterScores();
-  const { mostRecentJetDashVrScores } = await getMostRecentJetDashVrScores();
-  const { mostRecentGhostHunterScores } = await getMostRecentGhostHunterScores();
   const { reputation } = await getReputation();
 
   const data = {
-    jetDashVrScores,
     lastRefreshed: toFriendlyDate(new Date()),
     lastUpdatedRepos,
     latestReleases,
     spotify: `https://spotify-github-profile.kittinanx.com/api/view?uid=${process.env.SPOTIFY_UID}&cover_image=true&theme=spotify-embed`,
-    ghostHunterScores,
-    mostRecentJetDashVrScores,
-    mostRecentGhostHunterScores,
     reputation
   };
 
